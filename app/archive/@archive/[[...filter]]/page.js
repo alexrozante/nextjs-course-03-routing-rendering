9ef1c75167a70fdf3d6857726f92017307c3645a
Alexandre Rozante <alexrozante@hotmail.com>
2024-12-25 21:55:47 -0300

    filtering by year and month

---
+++ app/archive/@archive/[[...filter]]/page.js
@@ -0,0 +1,54 @@
+import {
+  getAllNews,
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/app/helpers/news";
+import NewsList from "@/components/news-list";
+import Link from "next/link";
+
+export default async function FilterNewsPage({ params }) {
+  const filters = (await params)?.filter;
+
+  let year = filters?.[0];
+  let month = filters?.[1];
+
+  let links = getAvailableNewsYears();
+  let newsList;
+  if (filters) {
+    if (year) {
+      links = getAvailableNewsMonths(year);
+      newsList = month ? getNewsForYearAndMonth(year, month) : getNewsForYear(year);
+    }
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          {year && month && (
+            <Link href={`/archive/${year}`}>{year}-{month}</Link>
+          )}
+          {year && !month && (
+            <Link href={`/archive`}>{year}</Link>
+          )}
+          {year && <p> </p>}
+          <ul>
+            {links.map((link) => (
+              <li key={link}>
+                {year && !month ? (
+                  <Link href={`/archive/${year}/${link}`}>{link}</Link>
+                ) : !year ? (
+                  <Link href={`/archive/${link}`}>{link}</Link>
+                ) : null}
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      {!newsList && <p>Please, select an year to search.</p>}
+      {newsList && <NewsList news={newsList} />}
+    </>
+  );
+}
